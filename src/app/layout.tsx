0f3b4bdcdaee92eacce2bc6fdccf0a305351f5a7
superkingsely <superkingsely@gmail.com>
2025-09-09 14:50:06 +0100

    google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RYKMXQF24P"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RYKMXQF24P');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
